bfcb040e45c4386597c7af19f0979f750b3819d0
Daniël de Kok <me@danieldk.eu>
2019-04-11 08:17:49 +0200

    Create and upload prebuilt binaries on tags

---
+++ ci/before_deploy.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -ex
+
+binaries="sticker-server sticker-tag sticker-train"
+
+cargo build --target "$TARGET" --release
+
+tmpdir="$(mktemp -d)"
+name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
+staging="${tmpdir}/${name}"
+mkdir "${staging}"
+out_dir="$(pwd)/deployment"
+mkdir "${out_dir}"
+
+# Copy binaries
+for binary in ${binaries}; do
+  cp "target/${TARGET}/release/${binary}" "${staging}/${binary}"
+done
+
+# Strip binaries.
+for binary in ${binaries}; do
+  strip "${staging}/${binary}"
+done
+
+cp {README.md,LICENSE,NOTICE} "${staging}/"
+
+if [ "${TARGET}" = "x86_64-unknown-linux-gnu" ]; then
+  tf_build="linux-x86_64"
+elif [ "${TARGET}" = "x86_64-apple-darwin" ]; then
+  tf_build="darwin-x86_64"
+else
+  >&2 echo "Cannot build release tarbal for target ${TARGET}"
+  exit 1
+fi
+
+tf_archive="libtensorflow-cpu-${tf_build}-1.13.1.tar.gz"
+
+# Add Tensorflow library
+curl -O https://storage.googleapis.com/tensorflow/libtensorflow/${tf_archive}
+mkdir tensorflow
+tar -zxf "${tf_archive}" -C tensorflow
+cp tensorflow/lib/*.so "${staging}"
+
+# Add binary directory to the search path.
+if [ "$TARGET" = "x86_64-unknown-linux-gnu" ]; then
+  for binary in ${binaries}; do
+    patchelf --set-rpath '$ORIGIN' "${staging}/${binary}"
+  done
+else
+  # Darwin
+  for binary in ${binaries}; do
+    install_name_tool -add_rpath @executable_path/. "${staging}/${binary}"
+  done
+fi
+
+
+( cd "${tmpdir}" && tar czf "${out_dir}/${name}.tar.gz" "${name}")
+
+rm -rf "${tmpdir}"
