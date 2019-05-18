c9525ac55f01b76532e934a2e75175c0e88eb947
Daniël de Kok <me@danieldk.eu>
2019-05-18 15:50:04 +0200

    Vendor protocol buffer definitions

---
+++ tf-proto/src/lib.rs
@@ -0,0 +1,54 @@
+#[allow(renamed_and_removed_lints)]
+mod allocation_description;
+
+#[allow(renamed_and_removed_lints)]
+mod attr_value;
+
+#[allow(renamed_and_removed_lints)]
+mod cluster;
+
+#[allow(renamed_and_removed_lints)]
+mod config;
+pub use crate::config::ConfigProto;
+
+#[allow(renamed_and_removed_lints)]
+mod cost_graph;
+
+#[allow(renamed_and_removed_lints)]
+mod debug;
+
+#[allow(renamed_and_removed_lints)]
+mod function;
+
+#[allow(renamed_and_removed_lints)]
+mod graph;
+
+#[allow(renamed_and_removed_lints)]
+mod node_def;
+
+#[allow(renamed_and_removed_lints)]
+mod op_def;
+
+#[allow(renamed_and_removed_lints)]
+mod resource_handle;
+
+#[allow(renamed_and_removed_lints)]
+mod rewriter_config;
+
+#[allow(renamed_and_removed_lints)]
+mod step_stats;
+
+#[allow(renamed_and_removed_lints)]
+mod tensor;
+
+#[allow(renamed_and_removed_lints)]
+mod tensor_description;
+
+#[allow(renamed_and_removed_lints)]
+mod tensor_shape;
+
+#[allow(renamed_and_removed_lints)]
+mod types;
+
+#[allow(renamed_and_removed_lints)]
+mod versions;
