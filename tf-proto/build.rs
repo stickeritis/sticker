b6e2fee55372bd1c2f561d213e649a942f41dd41
Daniël de Kok <me@danieldk.eu>
2019-05-18 15:50:04 +0200

    Vendor protocol buffer definitions

---
+++ tf-proto/build.rs
@@ -0,0 +1,31 @@
+fn main() {
+    #[cfg(feature = "proto-compile")]
+    protoc_rust::run(protoc_rust::Args {
+        out_dir: "src",
+        input: &[
+            "protos/tensorflow/core/protobuf/cluster.proto",
+            "protos/tensorflow/core/protobuf/config.proto",
+            "protos/tensorflow/core/protobuf/debug.proto",
+            "protos/tensorflow/core/protobuf/rewriter_config.proto",
+            "protos/tensorflow/core/framework/allocation_description.proto",
+            "protos/tensorflow/core/framework/attr_value.proto",
+            "protos/tensorflow/core/framework/cost_graph.proto",
+            "protos/tensorflow/core/framework/function.proto",
+            "protos/tensorflow/core/framework/graph.proto",
+            "protos/tensorflow/core/framework/node_def.proto",
+            "protos/tensorflow/core/framework/op_def.proto",
+            "protos/tensorflow/core/framework/resource_handle.proto",
+            "protos/tensorflow/core/framework/step_stats.proto",
+            "protos/tensorflow/core/framework/tensor.proto",
+            "protos/tensorflow/core/framework/tensor_description.proto",
+            "protos/tensorflow/core/framework/tensor_shape.proto",
+            "protos/tensorflow/core/framework/types.proto",
+            "protos/tensorflow/core/framework/versions.proto",
+        ],
+        includes: &["protos"],
+        customize: protoc_rust::Customize {
+            ..Default::default()
+        },
+    })
+    .expect("protoc");
+}
