ea971457e0ceae978c2cc05efb12be030c445091
Daniël de Kok <me@danieldk.eu>
2019-07-13 19:24:10 +0200

    Nix: update Tensorflow dependency to 1.14.0

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
 with import <nixpkgs> {};
 
 let
-  libtensorflow_1_13_1 = with pkgs; callPackage ./nix/libtensorflow {
+  danieldk = pkgs.callPackage (builtins.fetchTarball {
+    url = "https://git.sr.ht/~danieldk/nix-packages/archive/709c93a84504d558613bfc2538297ef2c532b890.tar.gz";
+    sha256 = "0jspqxz8yzghn4j3awiqz3f76my8slk3s5ckk3gfzvhq1p0wzp5m";
+  }) {};
+  libtensorflow-cpu = danieldk.libtensorflow_1_14_0;
+  libtensorflow-gpu = with pkgs; danieldk.libtensorflow_1_14_0.overrideAttrs (oldAttrs: rec {
     inherit (linuxPackages) nvidia_x11;
     cudatoolkit = cudatoolkit_10_0;
     cudnn = cudnn_cudatoolkit_10_0;
-  };
+  });
 in stdenv.mkDerivation rec {
   name = "sticker-env";
   env = buildEnv { name = name; paths = buildInputs; };
@@ -18,7 +23,7 @@ in stdenv.mkDerivation rec {
 
   buildInputs = [
     curl
-    libtensorflow_1_13_1
+    libtensorflow-cpu
     openssl
   ] ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
 
