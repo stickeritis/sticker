dfafe78544dea2d70296cd3348babd8139178256
Daniël de Kok <me@danieldk.eu>
2019-10-18 07:46:04 +0200

    Test with Rust stable, only run Clippy on Linux

--- ci/script.sh
+++ ci/script.sh
@@ -3,11 +3,14 @@
 set -ex
 
 cargo build --target ${TARGET}
+
+# Needs to run after build/check, so that protobuf files are generated.
+cargo fmt --all -- --check
+
 cargo test --target ${TARGET}
 
-# On Rust 1.32.0, we only care about passing tests.
-if rustc --version | grep -v "^rustc 1.32.0"; then
-  cargo fmt --all -- --check
+# On the lower-bound Rust, we only care about passing tests.
+if [ "$TRAVIS_RUST_VERSION" = "stable" ] && [ "$TARGET" = "x86_64-unknown-linux-gnu" ]; then
   cargo clippy -- -D warnings
 fi
 
