210684777b71885ebd5376b585cb8ae2975d9c0a
Daniël de Kok <me@danieldk.eu>
2019-11-11 09:18:34 +0100

    Update Nix shell environment to use libtensorflow 1.15.0

--- shell.nix
+++ shell.nix
@@ -1,16 +1,12 @@
 with import <nixpkgs> {};
 
 let
-  danieldk = pkgs.callPackage (builtins.fetchTarball {
-    url = "https://git.sr.ht/~danieldk/nix-packages/archive/f02c88bdd5e959d2d3a7a71c5c1208431f7107b9.tar.gz";
-    sha256 = "05ax116wy35jcnvy82jv38ig0ssivg4lwv2gi2rlqdsbl4bg3gbf";
+  danieldk = pkgs.callPackage (fetchFromGitHub {
+    owner = "danieldk";
+    repo = "nix-packages";
+    rev = "4464a255f5e0adca710f23318ba861bbfa408f8f";
+    sha256 = "0pg69q8dyybpa62clfa2krp6pnshali2nsbkkpnahcsjf9zq0nap";
   }) {};
-  libtensorflow-cpu = danieldk.libtensorflow_1_14_0;
-  libtensorflow-gpu = with pkgs; danieldk.libtensorflow_1_14_0.override {
-    inherit (linuxPackages) nvidia_x11;
-    cudatoolkit = cudatoolkit_10_0;
-    cudnn = cudnn_cudatoolkit_10_0;
-  };
 in stdenv.mkDerivation rec {
   name = "sticker-env";
   env = buildEnv { name = name; paths = buildInputs; };
@@ -23,7 +19,7 @@ in stdenv.mkDerivation rec {
 
   buildInputs = [
     curl
-    libtensorflow-cpu
+    danieldk.libtensorflow.v1_15_0
     openssl
   ] ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
 
